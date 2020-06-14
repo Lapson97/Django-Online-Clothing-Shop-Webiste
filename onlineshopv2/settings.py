6d9b552928670211ebfc06377b0c72fe31a9268b
Lapson97 <matlapinski1997@gmail.com>
2020-06-14 13:19:23 +0200

    Hid the passwords

--- onlineshopv2/settings.py
+++ onlineshopv2/settings.py
@@ -10,17 +10,29 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import json
 import os
+from django.core.exceptions import ImproperlyConfigured
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
+    secrets = json.load(secrets_file)
+
+def get_secret(setting, secrets=secrets):
+    """Get secret setting or fail with ImproperlyConfigured"""
+    try:
+        return secrets[setting]
+    except KeyError:
+        raise ImproperlyConfigured("Set the {} setting".format(setting))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '27-g0+0&fqwsjb)@_7=cbsy9b(v6$awz*!@%3oenh2u+k_9wb%'
+SECRET_KEY = get_secret('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'OnlineShop',
         'USER': 'postgres',
-        'PASSWORD': 'abka9561',
+        'PASSWORD': get_secret('password'),
         'HOST': 'localhost',
         'PORT': '5432'
     }
